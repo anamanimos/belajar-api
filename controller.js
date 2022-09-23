2aa77c7f97ce05c0ca5c4de2ce4d2da0c44e2862
anamanimos <cranam21@gmail.com>
2022-09-23 22:29:36 +0700

    get all user

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require("./database");
 exports.index = function (req, res) {
   response.ok("App running", res);
 };
+
+exports.getAllUser = function (req, res) {
+  connection.query("SELECT * FROM users", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
